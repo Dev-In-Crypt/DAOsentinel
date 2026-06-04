0d56b8c3eb90edd8036334fdd066dbd669723c9f
Dev-In-Crypt <dev@govwatch.xyz>
2026-06-04 18:25:43 +0200

    fix(emails): add 'import * as React' to email templates (tsx runner needs explicit React for JSX runtime) + use createElement in render helpers

--- src/server/email/render.ts
+++ src/server/email/render.ts
@@ -1,16 +1,17 @@
+import { createElement } from 'react';
 import { render } from '@react-email/components';
 import MagicLinkEmail, { type MagicLinkEmailProps } from '@/emails/MagicLinkEmail';
 import WhaleAlertEmail, { type WhaleAlertEmailProps } from '@/emails/WhaleAlertEmail';
 import WeeklyDigestEmail, { type WeeklyDigestEmailProps } from '@/emails/WeeklyDigestEmail';
 
 export async function renderMagicLink(p: MagicLinkEmailProps) {
-  return render(MagicLinkEmail(p));
+  return render(createElement(MagicLinkEmail, p));
 }
 
 export async function renderWhaleAlert(p: WhaleAlertEmailProps) {
-  return render(WhaleAlertEmail(p));
+  return render(createElement(WhaleAlertEmail, p));
 }
 
 export async function renderWeeklyDigest(p: WeeklyDigestEmailProps) {
-  return render(WeeklyDigestEmail(p));
+  return render(createElement(WeeklyDigestEmail, p));
 }
